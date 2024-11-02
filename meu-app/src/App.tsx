8442712d3083f3da2baced8207edb60a563bf12d
Victor Hugo <muzaovitao@gmail.com>
2024-11-01 22:59:35 -0300

    update
    configuração para o funcionamento do projeto

---
+++ meu-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Meu App com Chakra UI!</h1>
+      <Button colorScheme="teal">Clique aqui</Button>
+    </div>
+  );
+};
+
+export default App;
