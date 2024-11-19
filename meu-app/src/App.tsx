d4a26c7ce465fb98daf2a4055675d86957b89eae
Victor Hugo <muzaovitao@gmail.com>
2024-11-18 21:53:01 -0300

    update
    começo de uma implementação da tela inicial

--- meu-app/src/App.tsx
+++ meu-app/src/App.tsx
@@ -1,12 +1,34 @@
-import { Button } from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import backgrounds from './assets/background-home.jpeg';
 
-const App = () => {
+export const App = () => {
+  console.log(backgrounds); // Utiliza a variável
   return (
-    <div>
-      <h1>Meu App com Chakra UI!</h1>
-      <Button colorScheme="teal">Clique aqui</Button>
-    </div>
-  );
-};
+    <div className='container'>
+      <Tabs align='center' variant='unstyled'>
+    <TabList>
+      <Tab _selected={{ color: 'white', bg: 'green.400' }}>Contact</Tab>
+      <Tab _selected={{ color: 'white', bg: 'green.400' }}>Home</Tab>
+      <Tab _selected={{ color: 'white', bg: 'green.400' }}>Projects</Tab>
+    </TabList>
+    <TabPanels>
+      <TabPanel>
+        <p>one!</p>
+      </TabPanel>
+      <TabPanel>
+        <div className='background_Home'>
+      <img  src='{backgrounds}' alt="abc"/>
+        <p>two!</p>
+        </div>
+
+      </TabPanel>
+      <TabPanel>
+        <p>three!</p>
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
+</div>
+  )
+}
 
 export default App;
