e67ccfdb51179fbbbb9703e1e6f36da9738eb7e4
Victor Hugo <muzaovitao@gmail.com>
2024-11-30 01:53:05 -0300

    update
    arrumaçao do codigo com o copilot

--- meu-app/src/App.tsx
+++ meu-app/src/App.tsx
@@ -1,49 +1,47 @@
-import { Avatar, WrapItem, Wrap } from '@chakra-ui/react'
-import { Heading, Stack } from '@chakra-ui/react'
+import { Avatar, WrapItem, Wrap, Heading, Stack, ChakraProvider } from '@chakra-ui/react';
 import './App.css';
 
 export const App = () => {
   return (
-    <div className='container'>
-      <div className='avatar'>
-        <Wrap>
-          <WrapItem >
-            <Avatar  size='2xl' name='Victor Hugo' src='https://bit.ly/sage-adebayo' />
-          </WrapItem>
-        </Wrap>
-        <div className='description'>
-          <Stack spacing={6}>
-            <Heading size='md' as='h4'>
-            Olá meu nome é Victor.
-            </Heading>
-              <Heading as='h6' size='xs'>
-              Estou cursando o 6º Semestre de Ciência da Computação,
-              e atualmente estou aprendendo e desenvolvendo meu conhecimento
-              front end utilizando o framework React.
-              </Heading>
-            </Stack>
-            </div>
-            <hr />
-            <div className='project'>
+    <ChakraProvider>
+      <div className='container'>
+        <div className='avatar'>
+          <Wrap>
+            <WrapItem>
+              <Avatar size='2xl' name='Victor Hugo' src='https://bit.ly/sage-adebayo' />
+            </WrapItem>
+          </Wrap>
+          <div className='description'>
             <Stack spacing={6}>
               <Heading size='md' as='h4'>
-              Projetos
+                Olá meu nome é Victor.
               </Heading>
-              </Stack>
-            </div>
-            <hr />
-          <div className='rodpe'>
-            <footer>
+              <Heading as='h6' size='xs'>
+                {/* Adicione mais conteúdo aqui se necessário */}
+              </Heading>
+            </Stack>
+          </div>
+        </div>
+        <div className="projetos-section">
+          <Stack spacing={6}>
+            <Heading size='md' as='h4'>
+              Projetos
+            </Heading>
+          </Stack>
+        </div>
+        <hr />
+        <div className="contatos-section">
+          <footer>
             <Stack spacing={6}>
               <Heading size='md' as='h4'>
                 Contatos
               </Heading>
             </Stack>
-            </footer>
+          </footer>
         </div>
-    </div>
-</div>
-  )
-}
+      </div>
+    </ChakraProvider>
+  );
+};
 
 export default App;
